65f318e33ca5df7f3212abf510706ae368b3bdfd
Vignesh Vijayaraghavan <vigvijay@indiana.edu>
2016-04-26 16:10:30 -0400

    to retrieve on a particular input drug or input file

--- src/main/java/com/ade/jedis/views/APIDelegator.java
+++ src/main/java/com/ade/jedis/views/APIDelegator.java
@@ -6,12 +6,14 @@ package com.ade.jedis.views;
 
 import com.ade.jedis.api.LoadingInitialDataDrugEventPair;
 import com.ade.jedis.api.LoadingInitialDataDrugIndicationDrugPair;
+import com.ade.jedis.api.RetrieveData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.beans.PropertyVetoException;
+import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.sql.SQLException;
@@ -30,6 +32,8 @@ public class APIDelegator {
     LoadingInitialDataDrugEventPair loadingInitialDataDrugEventPair;
     @Autowired
     LoadingInitialDataDrugIndicationDrugPair loadingInitialDataDrugIndicationDrugPair;
+    @Autowired
+    RetrieveData retrieveData;
 
     public void loadingInitialDataDrugEventPairStats(int databaseIndex,String fileAddress,String resultFileAddress) throws IOException, URISyntaxException, PropertyVetoException, SQLException, InterruptedException {
 
@@ -51,4 +55,19 @@ public class APIDelegator {
 
     }
 
+    public void retrieveData(String input, String result_files_address){
+        File dataFile = new File(input);
+        boolean finishedLoading = false;
+        if( !dataFile.isDirectory() && !dataFile.isHidden() && dataFile.canRead() && dataFile.exists())
+            finishedLoading = retrieveData.retrieveOnDruginFile(dataFile,result_files_address);
+        else
+            finishedLoading = retrieveData.retrieveOnDrugName(input);
+
+        if(finishedLoading){
+            System.out.println("Drug Retrieval Completed Successfully");
+        }
+        else{
+            System.out.println("Failure. Please check your input");
+        }
+    }
 }
